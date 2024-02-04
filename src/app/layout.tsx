14dca46d79ba6cbfae84412df000684ad229e78d
Aditya Raj <adistrim.dev@gmail.com>
2024-02-05 00:05:58 +0530

    modified tailwind class in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,8 +19,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={mons.className}>
-        <main className='font-mons antialiased max-w-2xl flex flex-col sm:flex-row mx-4 mt-4 lg:mx-auto'>
-          <div className='flex-auto min-w-0 mt-6 flex flex-col px-2 sm:px-0'>
+        <main className='font-mons antialiased max-w-2xl mx-4 mt-4 sm:mx-auto'>
+          <div className='flex-auto min-w-0 mt-6 flex flex-col px-2'>
             <Header />
             {children}
             <Footer />
