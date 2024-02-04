198930fb60d828e69222a1213234c31c13d97e18
Aditya Raj <adistrim.dev@gmail.com>
2024-02-04 22:55:55 +0530

    made header a component & and improved the interactiveness of the header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,10 @@
+import Header from "./components/header"
+
 export default function Home() {
   return (
     <main className='font-mons antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto'>
       <div className='flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0'>
-        <aside className='-ml-[8px] mb-16 tracking-tight'>
-          <div className="lg:sticky lg:top-20">
-            <nav className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative" id="nav">
-              <div className="flex flex-row space-x-0 pr-10">
-                <a className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2" href="/">home</a>
-                <a className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2" href="/work">work</a>
-                <a className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2" href="/blog">blog</a>
-              </div>
-            </nav>
-          </div>
-        </aside>
+        <Header />
         <section>
           <h1 className='font-medium text-2xl mb-8 tracking-tighter'>
             Hey, I'm Aditya Raj 👋
