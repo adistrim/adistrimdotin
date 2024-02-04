db3ad6035ec7f81870f485f5e3453845610c9e0d
Aditya Raj <adistrim.dev@gmail.com>
2024-02-04 23:04:09 +0530

    did some tinkering with the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import Header from './components/header'
 import './globals.css'
 
 // const inter = Inter({ subsets: ['latin'] })
-const  mons = Montserrat({ subsets: ['latin'] })
+const mons = Montserrat({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'adistrim',
@@ -21,9 +21,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={mons.className}>
-        <Header />
-        {children}
-        </body>
-    </html>
+        <main className='font-mons antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto'>
+          <div className='flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0'>
+            <Header />
+            {children}
+          </div>
+        </main>
+      </body>
+    </html >
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,17 @@ import Header from "./components/header"
 
 export default function Home() {
   return (
-    <main className='font-mons antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto'>
-      <div className='flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0'>
-        <Header />
-        <section>
-          <h1 className='font-medium text-2xl mb-8 tracking-tighter'>
-            Hey, I'm Aditya Raj 👋
-          </h1>
-          <p className='prose prose-neutral dark:prose-invert'>
-            Welcome to my personal website – a digital haven where I share my passions without social media algorithms. Explore and enjoy my curated digital living room.
-          </p>
-          <br />
-          <p className='prose prose-neutral dark:prose-invert'>
-            I'm a 3rd-year BTech Computer Science & Engineering Student, on the journey to becoming a Software Engineer. I have a keen interest in technology and aircrafts. I enjoy capturing moments through photography and delving into the complexities of our world.
-          </p>
-        </section>
-      </div>
-    </main>
+    <section>
+      <h1 className='font-medium text-2xl mb-8 tracking-tighter'>
+        Hey, I'm Aditya Raj 👋
+      </h1>
+      <p className='prose prose-neutral dark:prose-invert'>
+        Welcome to my personal website – a digital haven where I share my passions without social media algorithms. Explore and enjoy my curated digital living room.
+      </p>
+      <br />
+      <p className='prose prose-neutral dark:prose-invert'>
+        I'm a 3rd-year BTech Computer Science & Engineering Student, on the journey to becoming a Software Engineer. I have a keen interest in technology and aircrafts. I enjoy capturing moments through photography and delving into the complexities of our world.
+      </p>
+    </section>
   )
 }
