1042570cc80f584dc5dfa53deda083b5d6fc3734
Aditya Raj <adistrim.dev@gmail.com>
2024-02-09 12:42:58 +0530

    added some pakages and APIs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    env: {
+        NEXT_PUBLIC_YOUTUBE_API_KEY: process.env.NEXT_PUBLIC_YOUTUBE_API_KEY,
+        NEXT_PUBLIC_YOUTUBE_CHANNEL_ID: process.env.NEXT_PUBLIC_YOUTUBE_CHANNEL_ID,
+    }
 }
 
 module.exports = nextConfig
