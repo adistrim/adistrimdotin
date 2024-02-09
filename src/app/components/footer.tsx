64c4f1e2f2d5d30acde42f998ffbac49f2e44122
Aditya Raj <adistrim.dev@gmail.com>
2024-02-09 13:51:17 +0530

    added linkedin recomendation section changed the font to changa

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { FaGithub, FaLinkedin, FaTwitter, FaInstagram, FaEnvelope } from 'react-icons/fa';
+import { Changa } from "next/font/google";
+
+const changa = Changa({ subsets: ["latin"] })
 
 const Footer: React.FC = () => {
     return (
-        <footer className="mt-[5rem] p-0.5 sm:p-4 bg-gray-1000 text-white">
+        <footer className={`my-[1rem] mx-1 sm:p-0 bg-gray-1000 text-white ${changa.className}`}>
             <div className="flex items-center justify-between">
                 <div className="flex items-center space-x-2 sm:space-x-4">
                     <a href="https://github.com/adistrim" target="_blank" rel="noopener noreferrer" className="hover:text-gray-300" aria-label="GitHub"><FaGithub size={20} /></a>
@@ -12,7 +15,7 @@ const Footer: React.FC = () => {
                     <a href="https://instagram.com/adistrim" target="_blank" rel="noopener noreferrer" className="hover:text-gray-300" aria-label="Instagram"><FaInstagram size={20} /></a>
                     <a href="mailto:adistrim.dev@gmail.com" className="hover:text-gray-300" aria-label="Email"><FaEnvelope size={20} /></a>
                 </div>
-                <a href="https://github.com/adistrim/adistrim.me" target="_blank" rel="noopener noreferrer" className="hover:text-gray-300">Source Code</a>
+                <a href="https://github.com/adistrim/adistrim.me" target="_blank" rel="noopener noreferrer" className="hover:text-gray-500 italic text-gray-400 text-sm">Source Code</a>
             </div>
         </footer>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,41 @@
+import recommendationsData from "./data/recommendations.json";
+import RecommendationCard from "./components/linkedin";
+import { Changa } from "next/font/google";
+
+const changa = Changa({ subsets: ["latin"] })
+
 export default function Home() {
   return (
-    <section>
-      <h1 className='font-medium text-2xl mb-8 tracking-tighter'>
-        Hey, I'm Aditya Raj 👋
-      </h1>
-      <p className='prose prose-neutral dark:prose-invert'>
-        Welcome to my personal website – a digital haven where I share my passions without social media algorithms. Explore and enjoy my curated digital living room.
-      </p>
-      <br />
-      <h1 className='font-medium mt-4 text-xl mb-4 tracking-tighter'>
-        About me? 🤔
-      </h1>
-      <p className='prose prose-neutral dark:prose-invert'>
-        I'm a 3rd-year BTech Computer Science & Engineering Student, on the journey to becoming a Software Engineer. I have a keen interest in technology and aircrafts. I enjoy capturing moments through photography and delving into the complexities of our world.
-      </p>
-    </section>
-  )
+    <div className={`${changa.className} px-1`}>
+      <section>
+        <h1 className="font-medium text-2xl mb-8 tracking-tighter">
+          Hey, I'm Aditya Raj 👋
+        </h1>
+        <p className="prose prose-neutral dark:prose-invert">
+          Welcome to my personal website – a digital haven where I share my
+          passions without social media algorithms. Explore and enjoy my curated
+          digital living room.
+        </p>
+        <br />
+        <h1 className="font-medium my-4 text-xl tracking-tighter">
+          About me? 🤔
+        </h1>
+        <p className="prose prose-neutral dark:prose-invert">
+          I'm a 3rd-year BTech Computer Science & Engineering Student, on the
+          journey to becoming a Software Engineer. I have a keen interest in
+          technology and aircrafts. I enjoy capturing moments through
+          photography and delving into the complexities of our world.
+        </p>
+      </section>
+      <div className="my-10">
+        <h1 className="font-medium mt-4 text-xl mb-4 tracking-tighter">Recommendations</h1>
+        {recommendationsData.map((recommendation: any) => (
+          <RecommendationCard
+            key={recommendation.id}
+            recommendation={recommendation}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
