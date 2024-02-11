d577877ebcc76f8940985b5a68e3322867a8c4c3
Aditya Raj <adistrim.dev@gmail.com>
2024-02-11 10:56:04 +0530

    Added changelog modified the header and footer

---
+++ src/app/changelog/page.tsx
@@ -0,0 +1,76 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import moment from "moment";
+import { Changa } from "next/font/google";
+
+const changa = Changa({ subsets: ["latin"] })
+
+interface Commit {
+  sha: string;
+  commit: {
+    author: {
+      name: string;
+      date: string;
+    };
+    message: string;
+  };
+}
+
+const Changelog: React.FC = () => {
+  const [commits, setCommits] = useState<Commit[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    fetch("https://api.github.com/repos/adistrim/adistrim.me/commits")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch commits");
+        }
+        return response.json();
+      })
+      .then((data: Commit[]) => {
+        setCommits(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError("Failed to fetch commits. Please try again later.");
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className={`min-h-screen mb-20 ${changa.className} px-1`}>
+      <h2 className="font-medium text-xl mb-8 tracking-tighter">Changelog</h2>
+      {loading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>{error}</p>
+      ) : (
+        <ul>
+          {commits.map((commit) => (
+            <li key={commit.sha} className="my-[2rem]">
+              <div className="mb-2">
+                <strong>{commit.commit.author.name}</strong> committed on{" "}
+                {moment(commit.commit.author.date).format("MMMM D, YYYY")}:
+              </div>
+              <div className="mb-2">{commit.commit.message}</div>
+              <div>
+                <a
+                  href={`https://github.com/adistrim/adistrim.me/commit/${commit.sha}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-600 hover:underline"
+                >
+                  View on GitHub
+                </a>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Changelog;
--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -12,7 +12,6 @@ const Footer: React.FC = () => {
                     <a href="https://github.com/adistrim" target="_blank" rel="noopener noreferrer" className="hover:text-gray-300" aria-label="GitHub"><FaGithub size={20} /></a>
                     <a href="https://linkedin.com/in/adistrim" target="_blank" rel="noopener noreferrer" className="hover:text-gray-300" aria-label="LinkedIn"><FaLinkedin size={20} /></a>
                     <a href="https://twitter.com/_adistrim_" target="_blank" rel="noopener noreferrer" className="hover:text-gray-300" aria-label="Twitter"><FaTwitter size={20} /></a>
-                    <a href="https://instagram.com/adistrim" target="_blank" rel="noopener noreferrer" className="hover:text-gray-300" aria-label="Instagram"><FaInstagram size={20} /></a>
                     <a href="mailto:adistrim.dev@gmail.com" className="hover:text-gray-300" aria-label="Email"><FaEnvelope size={20} /></a>
                 </div>
                 <a href="https://github.com/adistrim/adistrim.me" target="_blank" rel="noopener noreferrer" className="hover:text-gray-500 italic text-gray-400 text-sm">Source Code</a>
