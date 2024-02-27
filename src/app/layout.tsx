aa5470de4c12458c171ade6c60915fe3bee82a94
Aditya Raj <adistrim.dev@gmail.com>
2024-02-28 02:45:34 +0530

    added metadata and link image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,9 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Aditya Raj',
-  description: `Aditya Raj's personal website`,
+  description: `I am a 3rd-year BTech Computer Science & Engineering Student at JKLU, Jaipur, and also an associate-alumni of IIT Gandhinagar, on a journey to becoming a Software Engineer. I have a keen interest in aviation and tech.`,
+  keywords:
+    "Aditya Raj, aditya raj, aditya, raj, adistrim, Adistrim, ADISTRIM, portfolio, aditya portfolio, adistrim portfolio",
 }
 
 export default function RootLayout({
@@ -15,6 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/me.png" />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
       <body>
         <main className='font-mons antialiased max-w-2xl mx-4 mt-4 sm:mx-auto'>
           <div className='flex-auto min-w-0 mt-6 flex flex-col px-2'>
