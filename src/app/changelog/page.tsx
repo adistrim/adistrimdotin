854fd92c103145b4867ddf5bd775fa89eb016b6b
Aditya Raj <adistrim.dev@gmail.com>
2024-02-29 12:56:09 +0530

    fixed the dark mode bug in the changelog page

--- src/app/changelog/page.tsx
+++ src/app/changelog/page.tsx
@@ -38,7 +38,7 @@ const Changelog: React.FC = () => {
 
   return (
     <div className="min-h-screen mb-20 px-1">
-      <h2 className="font-medium text-xl mb-8 tracking-tighter">Changelog</h2>
+      <h2 className="font-medium text-xl dark:text-gray-200 mb-8 tracking-tighter">Changelog</h2>
       {loading ? (
         <p>Loading...</p>
       ) : error ? (
@@ -46,12 +46,12 @@ const Changelog: React.FC = () => {
       ) : (
         <ul>
           {commits.map((commit) => (
-            <li key={commit.sha} className="my-[2rem]">
+            <li key={commit.sha} className="my-[2rem] dark:text-gray-200">
               <div className="mb-2">
                 <strong>{commit.commit.author.name}</strong> committed on{" "}
                 {moment(commit.commit.author.date).format("MMMM D, YYYY")}:
               </div>
-              <div className="mb-2">{commit.commit.message}</div>
+              <div className="mb-2 dark:text-gray-400">{commit.commit.message}</div>
               <div>
                 <a
                   href={`https://github.com/adistrim/adistrim.me/commit/${commit.sha}`}
--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -16,7 +16,7 @@ const Header: React.FC = () => {
             <div className="lg:sticky lg:top-20">
                 <nav className="flex flex-row items-start justify-between relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative" id="nav">
                     <div className="flex flex-row space-x-1 pr-10">
-                        <a className={`transition-all hover:bg-neutral-200 dark:hover:bg-gray-800 hover:text-gray-700 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 rounded-lg ${pathname === '/' ? 'bg-neutral-200 dark:bg-gray-800 dark:text-white dark:hover:text-neutral-200' : ''}`} href="/">home</a>
+                        <a className={`transition-all hover:bg-neutral-200 dark:hover:bg-gray-800 hover:text-gray-700 dark:text-gray-200 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 rounded-lg ${pathname === '/' ? 'bg-neutral-200 dark:bg-gray-800 dark:text-white dark:hover:text-neutral-200' : ''}`} href="/">home</a>
                         {/* <a className={`transition-all hover:bg-gray-800 hover:text-neutral-200 flex align-middle relative py-1 px-2 rounded-lg ${pathname === '/work' ? 'bg-gray-800 text-neutral-200' : ''}`} href="/work">work</a> */}
                     </div>
                     {/* <div>
