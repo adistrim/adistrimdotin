25f3b37973103bcb5be686a40230606d71d56d36
Aditya Raj <adistrim.dev@gmail.com>
2024-08-28 14:38:37 +0530

    updated metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,18 @@
-import type { Metadata } from 'next';
-import Header from './components/header';
-import Footer from './components/footer';
-import './globals.css';
+import type { Metadata } from "next";
+import Header from "./components/header";
+import Footer from "./components/footer";
+import "./globals.css";
 import React from "react";
 import { Changa } from "next/font/google";
-import GoogleAnalytics from './components/GoogleAnalytics';
+import GoogleAnalytics from "./components/GoogleAnalytics";
 
 const changa = Changa({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Aditya Raj',
-  description: `I'm a 3rd-year BTech Computer Science & Engineering Student at JKLU, Jaipur, and also an associate-alumni of IIT Gandhinagar, on a journey to becoming a Software Engineer. I have a keen interest in aviation and tech.`,
-  keywords: "Aditya Raj, aditya raj, aditya, raj, adistrim, Adistrim, ADISTRIM, portfolio, aditya portfolio, adistrim portfolio",
+  title: "Aditya Raj",
+  description: `A digital space where I share my passions without social media algorithms`,
+  keywords:
+    "Aditya Raj, aditya raj, aditya, raj, adistrim, Adistrim, ADISTRIM, portfolio, aditya portfolio, adistrim portfolio",
 };
 
 const setInitialTheme = `
@@ -26,7 +27,11 @@ const setInitialTheme = `
   })();
 `;
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <GoogleAnalytics />
@@ -37,8 +42,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <script dangerouslySetInnerHTML={{ __html: setInitialTheme }} />
       </head>
       <body className="bg-[#fefbf6] dark:bg-[#111010]">
-        <main className={`${changa.className} antialiasing max-w-3xl mx-4 mt-4 sm:mx-auto`}>
-          <div className='flex-auto min-w-0 mt-6 flex flex-col'>
+        <main
+          className={`${changa.className} antialiasing max-w-3xl mx-4 mt-4 sm:mx-auto`}
+        >
+          <div className="flex-auto min-w-0 mt-6 flex flex-col">
             <Header />
             {children}
             <Footer />
