70b0137535765ca49fdaba9defd72f0e0b909bd0
Aditya Raj <adistrim.dev@gmail.com>
2025-01-07 14:54:07 +0530

    added new components to toggle theme

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -1,43 +1,19 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
-import { usePathname } from "next/navigation";
-import { Changa } from "next/font/google";
-import { FiSun, FiMoon } from "react-icons/fi";
+import React from "react";
 import Link from "next/link";
-import useTheme from "../../hooks/useTheme";
+import { usePathname } from "next/navigation";
+import ModeToggle from "@/components/theme-toggle";
 
-const changa = Changa({ subsets: ["latin"] });
-
-interface ThemeState {
-  theme: 'light' | 'dark';
-  toggleTheme: () => void;
-}
-
-const Header: React.FC = () => {
+const Header = React.memo(() => {
   const pathname = usePathname();
-  const { theme, toggleTheme } = useTheme() as ThemeState;
-  const [isMounted, setIsMounted] = useState(false);
-
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
 
   const navItems = [
-    { path: "/", label: "home" },
-    { path: "/blog", label: "blog" },
+    { path: "/", label: "Home" },
+    { path: "/blog", label: "Blogs" },
   ];
 
   const getLinkClassName = (path: string) => `
-    transition-all 
-    hover:bg-neutral-200 
-    dark:hover:bg-gray-800 
-    hover:text-gray-700 
-    dark:text-gray-200 
-    dark:hover:text-neutral-200 
-    flex 
-    align-middle 
-    relative 
     py-1 
     px-2 
     rounded-lg
@@ -45,7 +21,7 @@ const Header: React.FC = () => {
   `;
 
   return (
-    <aside className={`mb-10 tracking-tight ${changa.className}`}>
+    <aside className="mb-10 tracking-tight">
       <div className="lg:sticky lg:top-20">
         <nav
           className="flex flex-row items-start justify-between relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
@@ -65,28 +41,12 @@ const Header: React.FC = () => {
             ))}
           </div>
           <div>
-            {isMounted && (
-              <button
-                className="rounded-full p-2 text-gray-700 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white transition-colors"
-                onClick={toggleTheme}
-                aria-label={
-                  theme === "dark"
-                    ? "Switch to light mode"
-                    : "Switch to dark mode"
-                }
-              >
-                {theme === "dark" ? (
-                  <FiMoon size={20} aria-hidden="true" />
-                ) : (
-                  <FiSun size={20} aria-hidden="true" />
-                )}
-              </button>
-            )}
+            <ModeToggle />
           </div>
         </nav>
       </div>
     </aside>
   );
-};
+});
 
 export default Header;
