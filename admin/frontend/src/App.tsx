ce00ce88741b91f8820fc735ed15bd064edd316a
adistrim <adistrim.dev@gmail.com>
2025-01-19 08:26:07 +0530

    added vite-frontend for the admin zone

---
+++ admin/frontend/src/App.tsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-3xl">
+        adistrim | Admin
+      </h1>
+    </div>
+  )
+}
+
