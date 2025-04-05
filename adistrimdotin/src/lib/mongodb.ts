9638b634dd3437fc49ce56c70164aa614b241d3b
Aditya Raj <adistrim.dev@gmail.com>
2025-04-05 12:06:17 +0530

    removed build error handler

--- adistrimdotin/src/lib/mongodb.ts
+++ adistrimdotin/src/lib/mongodb.ts
@@ -13,20 +13,19 @@ declare global {
   var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
+if (!connectionStr) {
+  throw new Error('MONGODB_URI is not defined. Make sure it is set in the environment.');
+}
+
 if (process.env.NODE_ENV === "development") {
-  // In development mode, use a global variable so that the value
-  // is preserved across module reloads caused by HMR (Hot Module Replacement).
   if (!global._mongoClientPromise) {
     client = new MongoClient(connectionStr, options);
     global._mongoClientPromise = client.connect();
   }
   clientPromise = global._mongoClientPromise;
 } else {
-  // In production mode, it's best to not use a global variable.
   client = new MongoClient(connectionStr, options);
   clientPromise = client.connect();
 }
 
-// Export a module-scoped MongoClient promise. By doing this in a
-// separate module, the client can be shared across functions.
 export default clientPromise;
--- adistrimdotin/src/utils/db.ts
+++ adistrimdotin/src/utils/db.ts
@@ -1,5 +1,1 @@
-if (!process.env.MONGODB_URI) {
-    throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
-}
-
-export const connectionStr: string = process.env.MONGODB_URI;
+export const connectionStr: string | undefined = process.env.MONGODB_URI;
