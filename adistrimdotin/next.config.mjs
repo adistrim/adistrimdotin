b3a449834fffab172e5543e445f20c369c409685
Aditya Raj <adistrim.dev@gmail.com>
2025-04-20 13:12:15 +0530

    replaced deprecated images.domains with remotePatterns

--- adistrimdotin/next.config.mjs
+++ adistrimdotin/next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["cdn.hashnode.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.hashnode.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
