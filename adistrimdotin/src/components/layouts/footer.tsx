767f43beb0265ec421cfeb2c3056f8debeff4297
Aditya Raj <adistrim.dev@gmail.com>
2025-04-27 12:14:15 +0530

    refactoring in footer.tsx - removed lucide-react

--- adistrimdotin/src/components/layouts/footer.tsx
+++ adistrimdotin/src/components/layouts/footer.tsx
@@ -1,34 +1,75 @@
-"use client";
-import React from "react";
-import { usePathname } from 'next/navigation';
-import { FaGithub, FaLinkedin, FaTwitter, FaEnvelope } from 'react-icons/fa';
-import { FaHashnode } from "react-icons/fa6";
-import { Changa } from "next/font/google";
+import {
+  FaGithub,
+  FaLinkedin,
+  FaEnvelope,
+  FaHashnode,
+  FaXTwitter,
+} from "react-icons/fa6";
+import Link from "next/link";
 
-const changa = Changa({ subsets: ["latin"] });
-
-const Footer: React.FC = () => {
-    const pathname = usePathname();
-
-    return (
-        <footer className={`my-[1rem] mx-1 sm:p-0 bg-gray-1000 text-white ${changa.className}`}>
-            <div className="flex mb-[15rem] items-center justify-between">
-                <div className="flex items-center space-x-2 sm:space-x-4">
-                    <a href="https://adistrim.hashnode.dev/" className="text-neutral-500 dark:text-gray-300 hover:text-neutral-400 dark:hover:text-gray-200" aria-label="Email"><FaHashnode size={20} /></a>
-                    <a href="https://github.com/adistrim" target="_blank" rel="noopener noreferrer" className="text-neutral-500 dark:text-gray-300 hover:text-neutral-400 dark:hover:text-gray-200" aria-label="GitHub"><FaGithub size={20} /></a>
-                    <a href="https://linkedin.com/in/adistrim" target="_blank" rel="noopener noreferrer" className="text-neutral-500 dark:text-gray-300 hover:text-neutral-400 dark:hover:text-gray-200" aria-label="LinkedIn"><FaLinkedin size={20} /></a>
-                    <a href="https://twitter.com/_adistrim_" target="_blank" rel="noopener noreferrer" className="text-neutral-500 dark:text-gray-300 hover:text-neutral-400 dark:hover:text-gray-200" aria-label="Twitter"><FaTwitter size={20} /></a>
-                    <a href="mailto:adistrim.dev@gmail.com" className="text-neutral-500 dark:text-gray-300 hover:text-neutral-400 dark:hover:text-gray-200" aria-label="Email"><FaEnvelope size={20} /></a>
-                </div>
-                <a href="/changelog" rel="noopener noreferrer" className="hover:text-gray-400 italic text-gray-500 text-base" tabIndex={0}>Changelog</a>
-            </div>
-            <div className="flex items-center justify-center mt-4">
-                <p className="text-gray-500 dark:text-gray-300 italic text-base">
-                    © 2024 Aditya Raj (adistrim)
-                </p>
-            </div>
-        </footer>
-    )
-}
+const Footer = () => {
+  return (
+    <footer className="my-[1rem] mx-1 sm:p-0 bg-gray-1000 text-white">
+      <div className="flex mb-[15rem] items-center justify-between">
+        <div className="flex items-center space-x-2 sm:space-x-4">
+          <Link
+            href="https://blog.adistrim.in/"
+            className="text-neutral-500 dark:text-gray-300 hover:text-neutral-400 dark:hover:text-gray-200"
+            aria-label="Email"
+          >
+            <FaHashnode size={20} />
+          </Link>
+          <Link
+            href="https://github.com/adistrim"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-neutral-500 dark:text-gray-300 hover:text-neutral-400 dark:hover:text-gray-200"
+            aria-label="GitHub"
+          >
+            <FaGithub size={20} />
+          </Link>
+          <Link
+            href="https://linkedin.com/in/adistrim"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-neutral-500 dark:text-gray-300 hover:text-neutral-400 dark:hover:text-gray-200"
+            aria-label="LinkedIn"
+          >
+            <FaLinkedin size={20} />
+          </Link>
+          <Link
+            href="https://twitter.com/_adistrim_"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-neutral-500 dark:text-gray-300 hover:text-neutral-400 dark:hover:text-gray-200"
+            aria-label="Twitter"
+          >
+            <FaXTwitter size={20} />
+          </Link>
+          <Link
+            href="mailto:araj@adistrim.in"
+            className="text-neutral-500 dark:text-gray-300 hover:text-neutral-400 dark:hover:text-gray-200"
+            aria-label="Email"
+          >
+            <FaEnvelope size={20} />
+          </Link>
+        </div>
+        <Link
+          href="/changelog"
+          rel="noopener noreferrer"
+          className="hover:text-gray-400 italic text-gray-500 text-base"
+          tabIndex={0}
+        >
+          Changelog
+        </Link>
+      </div>
+      <div className="flex items-center justify-center mt-4">
+        <p className="text-gray-500 dark:text-gray-300 italic text-base">
+          © 2024 Aditya Raj (adistrim)
+        </p>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
