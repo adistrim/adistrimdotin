4b2f52cf68657f7e909458e276f8def27765ec5b
Aditya Raj <adistrim.dev@gmail.com>
2025-04-27 12:17:27 +0530

    removed lucide-react from theme-toggle

--- adistrimdotin/src/components/theme-toggle.tsx
+++ adistrimdotin/src/components/theme-toggle.tsx
@@ -1,23 +1,21 @@
-"use client"
+import { FaRegMoon } from "react-icons/fa";
+import { FiSun } from "react-icons/fi";
 
-import * as React from "react"
-import { Moon, Sun } from "lucide-react"
-import { useTheme } from "next-themes"
+import { useTheme } from "next-themes";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 
 export default function ModeToggle() {
-    const { theme, setTheme } = useTheme()
+  const { theme, setTheme } = useTheme();
 
-    const toggleTheme = () => {
-        setTheme(theme === "light" ? "dark" : "light")
-    }
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
 
-    return (
-        <Button variant="ghost" size="icon" onClick={toggleTheme}>
-            <Sun className="h-[1.3rem] w-[1.3rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-            <Moon className="absolute h-[1.3rem] w-[1.3rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-            <span className="sr-only">Toggle theme</span>
-        </Button>
-    )
+  return (
+    <Button variant="ghost" size="icon" onClick={toggleTheme}>
+      <FiSun className="h-[1.3rem] w-[1.3rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <FaRegMoon className="absolute h-[1.3rem] w-[1.3rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+    </Button>
+  );
 }
