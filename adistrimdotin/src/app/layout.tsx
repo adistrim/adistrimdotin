1ee8995f84e496fbd53a05e57853f73ad213ef71
adistrim <adistrim.dev@gmail.com>
2025-04-30 23:14:15 +0530

    added simpleanalytics script

--- adistrimdotin/src/app/layout.tsx
+++ adistrimdotin/src/app/layout.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import Header from "@/components/layouts/header";
 import Footer from "@/components/layouts/footer";
 import { siteMetadata } from "./metadata";
+import Script from "next/script";
 
 export const metadata = siteMetadata;
 const changa = Changa({ subsets: ["latin"] });
@@ -19,12 +20,21 @@ export default function RootLayout({
       <body
         className={`min-h-screen bg-background ${changa.className} max-w-3xl mx-4 mt-4 sm:mx-auto`}
       >
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
           <Header />
           {children}
           <Footer />
         </ThemeProvider>
       </body>
+      <Script
+        data-collect-dnt="true"
+        src="https://scripts.simpleanalyticscdn.com/latest.js"
+      />
     </html>
   );
 }
