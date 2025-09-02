bcf62f5643689dcd964a2ae000b46d6bd91672ad
Aditya Raj <adistrim.dev@gmail.com>
2025-09-02 21:05:10 +0530

    update theme-toggle button variant

--- adistrimdotin/src/components/theme-toggle.tsx
+++ adistrimdotin/src/components/theme-toggle.tsx
@@ -13,7 +13,7 @@ export default function ModeToggle() {
   };
 
   return (
-    <Button variant="ghost" size="icon" onClick={toggleTheme}>
+    <Button variant="outline" size="icon" onClick={toggleTheme}>
       <FiSun className="h-[1.3rem] w-[1.3rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <FaRegMoon className="absolute h-[1.3rem] w-[1.3rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
     </Button>
