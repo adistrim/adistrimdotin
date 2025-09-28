e77f8ddfccd0e028727f843822d79a80442b9211
Aditya Raj <adistrim.dev@gmail.com>
2025-09-29 00:36:40 +0530

    refactor root page

--- adistrimdotin/src/app/page.tsx
+++ adistrimdotin/src/app/page.tsx
@@ -4,18 +4,10 @@ import Projects from "@/components/sections/Projects/Projects";
 
 export default function Home() {
   return (
-    <main className="min-h-screen">
-      <section className="grid grid-cols-1 md:grid-cols-3">
-        <AboutSection />
-      </section>
-
-      <section className="mx-auto my-15">
-        <SocialLinksSection />
-      </section>
-
-      <section className="mx-auto my-10">
-        <Projects />
-      </section>
+    <main>
+      <AboutSection />
+      <SocialLinksSection />
+      <Projects />
     </main>
   );
 }
