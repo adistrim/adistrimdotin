657756c2a2b761180dbac79bb9953584e27d107e
Aditya <98259698+adistrim@users.noreply.github.com>
2025-11-18 23:18:51 +0530

    perf optimizations (#3)
    * schema -> models
    
    * add .tokeignore
    
    * add constants
    
    * refactor: update getBlogs implementation
    
    * bio update
    
    * latestBlogPost: minor UI changes
    
    * add social links data
    
    * fix: fix build issues

--- adistrimdotin/src/app/api/recommendations/route.ts
+++ adistrimdotin/src/app/api/recommendations/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { dbConnect } from "@/utils/db";
-import { Recommendation } from "@/schema/recommendations";
+import { Recommendation } from "@/models/recommendations";
 
 export const revalidate = 86400; // 86400 = 24 hours (revalidate every 24 hours)
 
---
+++ adistrimdotin/src/config.ts
@@ -0,0 +1,4 @@
+export const ENV = {
+  databaseUrl: process.env.MONGODB_URI,
+  hashnodeApiKey: process.env.NEXT_PUBLIC_HASHNODE,
+}
