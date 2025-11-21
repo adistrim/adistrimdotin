cfb2939e3a081678a1bfd6e66dd6d16aa626b6be
adistrim <adistrim.dev@zohomail.in>
2025-11-21 15:06:35 +0530

    restructure

--- admin/src/app/admin/changelog/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Changelog() {
-  return (
-    <div className="flex min-h-svh justify-center items-center">
-      <p>Coming Soon</p>
-    </div>
-  );
-}
--- admin/src/app/admin/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Admin() {
-  return (
-    <div className="flex min-h-svh justify-center items-center">
-      <p>Admin home</p>
-    </div>
-  );
-}
