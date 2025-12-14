92ec846d1865f2a7226ee25751deb18855bfde32
adistrim <adistrim.dev@zohomail.in>
2025-12-14 19:57:20 +0530

    Bump dependencies

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -43,11 +43,12 @@ export interface ButtonProps
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
+    const { popover, ...rest } = props
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
-        {...props}
+        {...rest}
       />
     )
   }
